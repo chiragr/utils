e30ef2107b1e68d1b392afe6b27518de0d57eadf
Chirag Rathod <chirag.rathod@oracle.com>
2013-11-11 11:53:24 +0530

    Code refactor.

--- README.md
+++ README.md
@@ -9,4 +9,10 @@ Built to be used with NodeJS, mailer.js sends personalized emails to a group of 
 
 Emails have a customized message as well as common message.
 
-Great utility for sending farewell/keep_in_touch emails while resigning from a job. 
+Great utility for sending farewell/keep_in_touch emails while resigning from a job! :-)
+
+Steps to execute:
+  1. Install node.js (http://nodejs.org/)
+  2. Copy this file to a directory of your choice E.g.: ~/mailer.js
+  3. Update sections that have comments starting with "Setup"
+  4. Execute: node mailer.js
--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,102 +1,121 @@
-var nodemailer = require('nodemailer');
+/*
+ * mailer.js
+ * 
+ * Email utility that uses nodeJS to send customized bulk email.
+ * 
+ * Steps to execute:
+ * 1. Install node.js (http://nodejs.org/)
+ * 2. Copy this file to a directory of your choice E.g.: ~/mailer.js
+ * 3. Update sections that have comments starting with "Setup"
+ * 4. Execute: node mailer.js
+ * 
+ */
+(function(){
+    var nodemailer = require("nodemailer");
 
-// Setup recipients
-var mailTo =
-{
-    "john.doe@gmail.com" :
+    // Setup Mail Server
+    var smtpServer     = "smtp.gmail.com";
+    var smtpPort       = 465;
+    var isConnSecure   = true;
+    var senderLogin    = "john.doe@gmail.com";
+    var senderPassword = "ItsASecret";
+
+    // Setup Sender Name
+    var senderName = "John Doe<john.doe@gmail.com>";
+
+    // Setup Recipients
+    var mailTo =
     {
-        "address" : "Hi ", // You can use Dear here if you like! :)
-        "name" : "Jonny,", // Name
-        "customMessage1" : "Hope you are doing great!",  // Custom message that will appear before commonMessageBody1
-        "customMessage2" : "I will definitely miss you!" // custom message that will appear after commonMessageBody1 and before commonMessageBody2
-    },
-    "jane.doe@gmail.com" :
-    {
-        "address" : "Dear ",
-        "name" : "Jinny,",
-        "customMessage1" : "Hope you are doing great!",
-        "customMessage2" : "I will definitely miss you while having lunch!"
-    }
-    // As as many recipients as you like!
-};
-
-// Subject
-var commonMesssageSubject = "Hey! Bye for now!";
-
-// Message Body. Split into two.
-var commonMessageBody1 = "Will not be around for few months.\n\nPing me if you need something.";
-var commonMessageBody2 = "Contact me on my cell phone.\n\nThank you!\nChirag";
-
-// Configure SMTP Server
-var smtpTransport = nodemailer.createTransport("SMTP",{
-            host: 'smtp.gmail.com',    // SMTP server
-            secureConnection: true,    // Secure. TTL/SSL required.
-            port: 465,                 // Secure port
-            auth: {
-              user: "me.my@gmail.com", // Sender's username
-              pass: "password"         // Sender's password
-            }
-});
-
-// Loop thru our recipient list and send individual mails.
-for (var email in mailTo)
-{
-    var recipientDetails = mailTo[email];
-
-    // Create message body using the fragments.
-    var messageBody = "";
-
-    if (recipientDetails.address && recipientDetails.address.length > 0)
-    {
-        messageBody = messageBody + recipientDetails.address;
-    }
-
-    if (recipientDetails.name && recipientDetails.name.length > 0)
-    {
-        messageBody = messageBody + recipientDetails.name + "\n\n";
-    }
-
-    if (recipientDetails.customMessage1 && recipientDetails.customMessage1.length > 0)
-    {
-        messageBody = messageBody + recipientDetails.customMessage1 + "\n\n";
-    }
-
-    if (commonMessageBody1 && commonMessageBody1.length > 0)
-    {
-        messageBody = messageBody + commonMessageBody1;
-    }
-
-    if (recipientDetails.customMessage2 && recipientDetails.customMessage2.length > 0)
-    {
-        messageBody = messageBody + recipientDetails.customMessage2 + "\n\n";
-    }
-
-    if (commonMessageBody2 && commonMessageBody2.length > 0)
-    {
-        messageBody = messageBody + commonMessageBody2;
-    }
-
-    // Setup e-mail data
-    var mailOptions =
-    {
-        from: "Chirag <me.my@gmail.com>", // Sender address
-        to: email,                        // Recipient address
-        subject: commonMesssageSubject,   // Subject line
-        text: messageBody                 // Plaintext body
+        "jane.doe@gmail.com" :
+        {
+            "address" : "hi ", // You can use Dear here if you like! :)
+            "name" : "Jane,",
+            "customMessage1" : "Hope you are doing great!",
+            "customMessage2" : "I will definitely miss you!"
+        }
+        // Add more recipeients here.
     };
 
-    // Send mail with defined transport object
-    smtpTransport.sendMail(mailOptions, function(error, response)
+    // Setup Subject
+    var commonMesssageSubject = "Bye ...";
+
+    // Setup Message Body
+    var commonMessage1 = "Tomorrow is my last day at Froogle. I had a great time here!\n\n";
+    var commonMessage2 = "Personal Mail ID: john.doe@gmail.com\nPersonal Web Page: http://johndoe.me\n\nPlease do keep in touch!\n\nThank you!\nJohn";
+
+    // Configure SMTP Server
+    var smtpTransport = nodemailer.createTransport("SMTP",
     {
-        if(error)
+        host: smtpServer,
+        secureConnection: isConnSecure,
+        port: smtpPort,
+        auth:
         {
-            console.log(error);
-        }
-        else
-        {
-            console.log("Message sent: " + response.message);
+            user: senderLogin,
+            pass: senderPassword
         }
     });
-}
 
-smtpTransport.close(); // Shut down the connection pool, no more messages
+    // Loop thru our recipient list and send individual mails.
+    for (var email in mailTo)
+    {
+        var recipientDetails = mailTo[email];
+
+        // Create message body using the fragments.
+        var messageBody = "";
+
+        if (recipientDetails.address && recipientDetails.address.length > 0)
+        {
+            messageBody = messageBody + recipientDetails.address;
+        }
+
+        if (recipientDetails.name && recipientDetails.name.length > 0)
+        {
+            messageBody = messageBody + recipientDetails.name + "\n\n";
+        }
+
+        if (recipientDetails.customMessage1 && recipientDetails.customMessage1.length > 0)
+        {
+            messageBody = messageBody + recipientDetails.customMessage1 + "\n\n";
+        }
+
+        if (commonMessage1 && commonMessage1.length > 0)
+        {
+            messageBody = messageBody + commonMessage1;
+        }
+
+        if (recipientDetails.customMessage2 && recipientDetails.customMessage2.length > 0)
+        {
+            messageBody = messageBody + recipientDetails.customMessage2 + "\n\n";
+        }
+
+        if (commonMessage2 && commonMessage2.length > 0)
+        {
+            messageBody = messageBody + commonMessage2;
+        }
+
+        // Email data
+        var mailOptions =
+        {
+            from: senderName,               // Sender address
+            to: email,                      // Recipient address
+            subject: commonMesssageSubject, // Subject line
+            text: messageBody               // Plaintext body
+        };
+
+        // Send mail with defined transport object
+        smtpTransport.sendMail(mailOptions, function(error, response)
+        {
+            if(error)
+            {
+                console.log(error);
+            }
+            else
+            {
+                console.log("Message sent: " + response.message);
+            }
+        });
+    }
+
+    smtpTransport.close(); // Shut down the connection pool
+})();
